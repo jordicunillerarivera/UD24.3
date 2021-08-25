815ab15b95b66730b9a038a1d88d637d4ea7ad8c
jordicunillerarivera <87013279+jordicunillerarivera@users.noreply.github.com>
2021-08-25 10:08:34 +0200

    Java y POM

--- bd.sql
+++ bd.sql
@@ -8,21 +8,24 @@ CREATE TABLE `UD26`.`piezas` (
   PRIMARY KEY (`codigo`));
 
 CREATE TABLE `UD26`.`proveedores` (
-  `id` CHAR(4) NOT NULL,
+  `id` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `UD26`.`suministra` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `codigoPieza` INT NOT NULL,
-  `idProveedor` CHAR(4) NOT NULL,
-  `precio` INT NULL,
-  PRIMARY KEY (`codigoPieza`, `idProveedor`),
-  INDEX `idProveedor_idx` (`idProveedor` ASC) VISIBLE,
-FOREIGN KEY (`codigoPieza`)
+  `idProveedor` INT NOT NULL,
+  `precio` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `piezas_idx` (`codigoPieza` ASC) VISIBLE,
+  INDEX `proveedores_idx` (`idProveedor` ASC) VISIBLE,
+    FOREIGN KEY (`codigoPieza`)
     REFERENCES `UD26`.`piezas` (`codigo`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-FOREIGN KEY (`idProveedor`)
+    FOREIGN KEY (`idProveedor`)
     REFERENCES `UD26`.`proveedores` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+
